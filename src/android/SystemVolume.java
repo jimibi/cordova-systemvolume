fec7c43c7802afcbb1a032454edb3828e0db2b84
Jean Marc BRUNO <technique@edps.fr>
2015-04-08 22:13:09 +0200

    PB INT RESOLU

--- src/android/SystemVolume.java
+++ src/android/SystemVolume.java
@@ -7,7 +7,6 @@ import org.json.JSONException;
 
 import android.content.Context;
 import android.media.AudioManager;
-import android.os.Vibrator;
 
 public class SystemVolume extends CordovaPlugin {
 	public SystemVolume(){
@@ -30,14 +29,8 @@ public class SystemVolume extends CordovaPlugin {
 
 	public void setSystemVolume(double volume) {
 		AudioManager am = (AudioManager) this.cordova.getActivity().getSystemService(Context.AUDIO_SERVICE);
-		int Level = (am.getStreamMaxVolume(AudioManager.STREAM_MUSIC) * volume)
-
-        	am.setStreamVolume(AudioManager.STREAM_MUSIC, Level, 0);
-		/*am.setStreamVolume(
-			AudioManager.STREAM_SYSTEM,
-			(int) (am.getStreamMaxVolume(AudioManager.STREAM_SYSTEM) * volume),
-			0);
-			*/
+		
+        am.setStreamVolume(AudioManager.STREAM_MUSIC, (int) (am.getStreamMaxVolume(AudioManager.STREAM_MUSIC) * volume), 0);
 	}
 	
 	public int getSystemVolume(int percent) {
@@ -46,10 +39,9 @@ public class SystemVolume extends CordovaPlugin {
 	        	int Level;
         		int max = am.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
     			int current =am.getStreamVolume(AudioManager.STREAM_MUSIC);
-			Level = Math.round(current / maxVolume);
+			Level = Math.round(current / max);
 			return Level;
 		}catch (Exception e) {
-		      Log.e(TAG, "Get volume error: " + e.getMessage(), e);
 		      return 1;	
 		}
 	}
